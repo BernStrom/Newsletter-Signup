d24f03860d1ab908be7d9a80d69a665a12ae96d8
Bern <bernnystrom@gmail.com>
2020-12-29 01:51:31 +0800

    Initial setup for mailchimp user subscription

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 const port = 3000;
+const apiKey = process.env.MAILCHIMP_API_KEY;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mailchimp.setConfig({
+    apiKey,
+    server: "us7"
+});
+
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.post("/", (req, res) => {
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let email = req.body.email;
 
-    
+    const listId = process.env.MAILCHIMP_AUDIENCE_LIST_ID;
+    const subscribingUser = {
+        firstName = req.body.firstName,
+        lastName = req.body.lastName,
+        email = req.body.email
+    };
+
 });
 
 app.listen(port, (req, res) => console.log(`Server running on port ${port}`));
