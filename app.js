166b29a279366f117adda4485a416cffd39ea893
Bern <bernnystrom@gmail.com>
2020-12-28 22:20:08 +0800

    Initialize the bodyParser and add a post request route with access to the form data save to their corresponding variables

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.post("/", (req, res) => {
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+
+    
+});
 
 app.listen(port, (req, res) => console.log(`Server running on port ${port}`));
