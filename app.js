84dad50632f5225d59d98ed003fc017f1fdbc5a5
Bern <bernnystrom@gmail.com>
2020-12-28 20:59:30 +0800

    Initial commit with node packages set up

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, (req, res) => console.log(`Server running on port ${port}`));
