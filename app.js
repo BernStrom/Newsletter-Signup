9c202960c538e1895d26f34146c7dd957056a691
Bern <bernnystrom@gmail.com>
2020-12-28 21:55:37 +0800

    Add GET request to retrieve the main page file for the root path

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(port, (req, res) => console.log(`Server running on port ${port}`));
